d1ee2a48648b3f5e529c53429c6c6a6515b869a6
Serge <neserge@gmail.com>
2022-09-30 19:11:37 +0300

    wip: oauth flow draft

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
-from typing import Any, Callable, Coroutine, Dict, Optional
+import collections
+from typing import Any, Callable, Coroutine, Dict, NamedTuple, Optional
+
+from evernote.api.client import EvernoteClient
 from fastapi import FastAPI
 
 from config import get_settings
+from commands import ping, auth
 from domain import process_message, get_message
 from entities import Update
 from telegram import Telegram
@@ -10,6 +14,23 @@ from telegram import Telegram
 settings = get_settings()
 telegram = Telegram(settings.telegram_token, settings.telegram_secret)
 app = FastAPI()
+storage = {}
+_connections = None
+
+
+def get_connections():
+    global _connections
+    if not _connections:
+        Connections = collections.namedtuple(
+            "Connections",
+            [
+                "telegram",
+                "storage",
+            ],
+        )
+        _connections = Connections(telegram=telegram, storage=storage)
+
+    return _connections
 
 
 @app.get("/healthcheck/")
@@ -17,18 +38,52 @@ async def healthcheck() -> Dict:
     return {"healthcheck": "OK"}
 
 
-async def ping(update: Update):
-    assert update.message is not None
-    assert update.message.chat is not None
+@app.get("/callback/{callback_id}")
+async def callback(
+    callback_id: str,
+    oauth_token: str | None = None,
+    oauth_verifier: str | None = None,
+    sandbox_lnb: bool | None = None,
+):
+    page_data = {
+        "callback_id": callback_id,
+        "oauth_token": oauth_token,
+        "oauth_verifier": oauth_verifier,
+        "sandbox_lnb": sandbox_lnb,
+    }
+    conns = get_connections()
+    if callback_id in conns.storage:
+        saved_data = conns.storage[callback_id]
+        print(saved_data, page_data)
 
-    PING_RESPONSE = "pong"
-    chat = update.message.chat
-    return await telegram.send_message(chat.id, PING_RESPONSE)
+        client = EvernoteClient(
+            consumer_key=settings.evernote_consumer_key,
+            consumer_secret=settings.evernote_consumer_secret,
+            sandbox=True,
+        )
+        access_token = client.get_access_token(
+            oauth_token=saved_data["oauth_token"],
+            oauth_token_secret=saved_data["oauth_token_secret"],
+            oauth_verifier=oauth_verifier,
+        )
+        evernote_user = client.get_user_store().getUser()
+        print(access_token)
+        await conns.telegram.send_message(
+            saved_data["chat_id"],
+            f"Hello, {evernote_user.username}!\nYou have been authorized",
+        )
+
+        return {**page_data, **saved_data, "username": evernote_user.username}
+
+    return page_data
 
 
 COMMANDS = {
     "/ping": ping,
+    "/auth": auth,
 }
+
+print(f"Installed commands: {COMMANDS.keys()}")
 
 
 def dispatch_command(
@@ -38,25 +93,30 @@ def dispatch_command(
         return None
 
     command = update.message.text
-    # Command if it's a oneliner starting with "/"
-    # And it is declared in COMMANDS
-    if not command:
-        return None
-    if not command.startswith("/"):
-        return None
-    if command.count("\n") > 0:
-        return None
-    if command in COMMANDS:
+    print(command)
+    if _is_command(command):
         return COMMANDS[command]
 
     return None
+
+
+def _is_command(command: Optional[str]) -> bool:
+    # Command if it's a oneliner starting with "/"
+    # And it is declared in COMMANDS
+    if not command:
+        return False
+    if not command.startswith("/"):
+        return False
+    if command.count("\n") > 0:
+        return False
+    return command in COMMANDS
 
 
 @app.post(telegram.get_webhook_url())
 async def webhook(update: Update):
     handler = dispatch_command(update)
     if handler:
-        await handler(update)
+        await handler(update, get_connections())
     else:
         message = get_message(update)
         process_message(message)
---
+++ src/commands.py
@@ -0,0 +1,46 @@
+from evernote.api.client import EvernoteClient
+
+from entities import Update
+from telegram import Telegram
+from config import get_settings
+
+
+async def ping(update: Update, conns):
+    assert update.message is not None
+    assert update.message.chat is not None
+
+    PING_RESPONSE = "pong"
+    chat = update.message.chat
+    return await conns.telegram.send_message(chat.id, PING_RESPONSE)
+
+
+async def auth(update: Update, conns):
+    assert update.message is not None
+    assert update.message.chat is not None
+
+    chat = update.message.chat
+    settings = get_settings()
+
+    callback_id = "randomToken123"
+    callback_url = "http://%s%s" % ("localhost:8000", f"/callback/{callback_id}")
+
+    client = EvernoteClient(
+        consumer_key=settings.evernote_consumer_key,
+        consumer_secret=settings.evernote_consumer_secret,
+        sandbox=True,
+    )
+    request_token = client.get_request_token(callback_url)
+    auth_url = client.get_authorize_url(request_token)
+
+    data = {
+        "chat_id": chat.id,
+        "oauth_token": request_token["oauth_token"],
+        "oauth_token_secret": request_token["oauth_token_secret"],
+    }
+    conns.storage[callback_id] = data
+
+    AUTH_RESPONSE = (
+        f"Auth for chat.id = {chat.id},\n"
+        f"Data you'll need: {data},\n\nGo to: {auth_url}"
+    )
+    return await conns.telegram.send_message(chat.id, AUTH_RESPONSE)
